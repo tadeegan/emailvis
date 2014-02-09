523baad1d10a88ad40df913ba52b5726619d7640
Thomas Deegan <tadeegan@gmail.com>
2014-02-09 17:25:30 -0600

    works

--- parse.py
+++ parse.py
@@ -1,18 +1,19 @@
 import mailbox
 import email
 import re
+import time
+import csv
 from email.utils import parsedate
 
 
-MBOX = 'smallmail.mbox'
+MBOX = 'mail.mbox'
 
 mbox = mailbox.mbox(MBOX)
 
+email = 'tadeegan@gmail.com'
 
 def extract_date(email):
-
     date = email['Date']
-    print date
     return parsedate(date)
 
 sorted_mails = sorted(mbox, key=extract_date)
@@ -29,13 +30,27 @@ for key , message in mbox.iteritems():
 array.sort(key=extract_date)
 
 regex = re.compile(re.escape('re: '), re.IGNORECASE)
+
+out = []
+
 for message in array:
     subject = message['subject']
     subject = regex.sub('', str(subject))
+    if subject == "":
+        continue
+    if email in str(message['to']):
+        lookup_table[subject] = message
+    else:
+        if not (subject in lookup_table):
+            continue
+        else:
+            prev_mes = lookup_table[subject]
+            sent_time = time.mktime(extract_date(prev_mes))
+            resp_time = time.mktime(extract_date(message))
+            diff =  resp_time - sent_time
+            out.append((sent_time, diff, "content"))
 
-    lookup_table[subject] = message
-    #print message['subject']
-    #print message['to']
-    #print message['from']
-    #print message.get_flags()
-    #print message['Date']
+with open('tsv.tsv', 'wb') as tsvfile:
+	csvwriter = csv.writer(tsvfile, delimiter='\t') 
+	for blah in out:
+		csvwriter.writerow(blah)
