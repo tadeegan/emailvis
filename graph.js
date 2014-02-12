6ba17d73a7d10e3ea8a693d812bf38a8b1288ad1
Thomas Deegan <tadeegan@gmail.com>
2014-02-12 00:18:16 -0600

    filtering and urgernt lines working

--- graph.js
+++ graph.js
@@ -4,10 +4,31 @@ var height = 600 - margin.top - margin.bottom;
 
 var DAYSTOSECONDS = 60*60*24;
 
+console.log(d3);
 var parseFullDate = d3.time.format("%m/%d/%Y %H:%M:%S %p").parse;
 //var parseMonthDate = d3.time.format("%m/%Y").parse;
 
-d3.tsv("tsv.tsv", function(error, data) {
+var filter = "ipad";
+
+function group(data, grouper){
+    return d3.nest()
+        .key(grouper)
+        .rollup(function(leaves) {
+            var a = {}; 
+            a.responseTime = d3.mean(leaves, function(leaf){return leaf.responseTime})
+            a.date = leaves[0].date;
+            return a;
+        })
+        .entries(data)
+        .map(function(entry){
+            return entry.values;
+        });
+}
+
+d3.tsv("nelson.tsv", function(error, data) {
+        data = data.filter(function(response){
+            return response.Content.toLowerCase().indexOf(filter) != -1;
+        });
         data.sort(function(d1, d2){
             return d3.ascending(d1.date, d2.date);
         });
@@ -17,28 +38,29 @@ d3.tsv("tsv.tsv", function(error, data) {
             d.date = parseFullDate(localDate.toLocaleString());
             d.responseTime = parseFloat(d.responseTime);
         });
-        var nest = d3.nest()
-            .key(function(d) {
-                var grouper = d.date.getMonth()+"/"+d.date.getYear();
-                return grouper;
-            })
-            .rollup(function(leaves) {
-                var a = {}; 
-                a.responseTime = d3.mean(leaves, function(leaf){return leaf.responseTime})
-                a.date = leaves[0].date;
-                return a;
-            })
-            .entries(data);
-        var yo = nest.map(function(entry){
-            return entry.values;
+
+        var urgent_data = data.filter(function(response){
+            var ret = response.urgent == "True"
+            return ret;
         });
+
+        data = group(data, function(d) {
+            var grouper = d.date.getMonth()+"/"+d.date.getYear();
+            return grouper;
+        });
+
+        urgent_data = group(urgent_data, function(d) {
+            var grouper = d.date.getMonth()+"/"+d.date.getYear();
+            return grouper;
+        });
+
         /*data.sort(function(d1, d2){
             return d3.ascending(d1.date, d2.date);
         });*/
-        render(yo);
+        render(data, urgent_data);
     });
 
-function render(dataset){
+function render(dataset, urgent_data){
 
     var maxResponseTime = d3.max(dataset, function(d){ return d.responseTime});
     var maxDate = d3.max(dataset, function(d){ return d.responseTime});
@@ -47,21 +69,28 @@ function render(dataset){
         .domain(d3.extent(dataset, function(d) {return d.date;}))
         .range([0, width]);
 
-    var y = d3.scale.linear()
-        .domain([0, maxResponseTime])
-        .range([height, 0]);
+    var y = d3.scale.log()
+        .domain([1, maxResponseTime])
+        .range([height, 1]);
 
     var xAxis = d3.svg.axis()
           .scale(x)
           .orient("bottom");
 
+    var formatHours = function(miliseconds) {
+        return (miliseconds / 1000 / 60 / 60).toFixed(2);     
+    }
+
     var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient("left");
+        .scale(y)
+        .orient("left")
+        .ticks(5)
+        .tickFormat(formatHours);
 
     var line = d3.svg.line()
-          .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(d.responseTime); });
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.responseTime); });
+
 
 
     var svg = d3.select("body").append("svg")
@@ -85,11 +114,16 @@ function render(dataset){
     .attr("y", 6)
     .attr("dy", ".71em")
     .style("text-anchor", "end")
-    .text("Response Time (ms)");
+    .text("Response Time (hours)");
 
     svg.append("path")
     .datum(dataset)
     .attr("class", "line")
+    .attr("d", line);
+
+    svg.append("path")
+    .datum(urgent_data)
+    .attr("class", "urgent-line")
     .attr("d", line);
 }
 
